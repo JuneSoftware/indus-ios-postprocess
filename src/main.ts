d9caa686fcc8a72881ac513268c05dc9dedc191c
Valiant John <84314269+ValiantJohnCh@users.noreply.github.com>
2022-07-23 18:21:18 +0530

    feat: Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import * as core from '@actions/core'
+import * as unzip from 'unzipper';
+import * as fs from 'fs';
+import * as fsExtra from 'fs-extra';
+import path from 'path';
+
+function run(): void {
+  const packagePath = core.getInput('zipPath');
+  const rootFolderName = core.getInput('zipRootName');
+  const outputPath = core.getInput('outputPath');
+  const tempPath = 'tempPath';
+  const tempPathResult = path.join(tempPath, rootFolderName);
+
+  fs.createReadStream(packagePath).pipe(unzip.Extract({ path: tempPath })).on('close', function () {
+    try {
+      move(tempPathResult, outputPath);
+    }
+    catch(ex) {
+      console.log(ex)
+    }
+    fsExtra.removeSync(tempPath);
+  });
+}
+
+function move(oldPath: string, newPath: string) {
+  if (!fsExtra.existsSync(newPath)) {
+    fsExtra.mkdirSync(newPath);
+  }
+
+  fsExtra.readdirSync(oldPath).forEach(file => {
+    console.log(file);
+    var filePath = path.join(oldPath, file);
+    if (!fsExtra.statSync(filePath).isDirectory()) {
+      var dest = path.join(newPath, file);
+      fsExtra.moveSync(filePath, dest);
+    }
+    else {
+      var dest = path.join(newPath, file);
+      move(filePath, dest);
+    }
+  });
+}
+
+run()
